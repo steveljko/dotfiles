583322e09ad313e2ca68b2706dd481967b5c4963
steveljko <steveljko@proton.me>
2024-08-07 10:50:00 +0200

    refector waybar tasks module to display only time

--- .config/waybar/modules/tasks.sh
+++ .config/waybar/modules/tasks.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-TASK=$(timew | awk -F'"' '/Tracking/ {print $2}')
+TASK=$(timew | awk '/Tracking/ {print $2}')
 TIME=$(timew | awk '/Total/ {print $2}')
 
-# output=$(jq -n \
-#   --arg task "$task" \
-#   --arg time "$time" \
-#   '{task: $task, time: $time}' | jq -c .)
-
 if [[ -n "$TASK" ]]; then
-  printf "{\"text\": \"%s [%s]\", \"tooltip\": \"%s\", \"alt\": \"%s\"}" \ "$TASK" "$TIME" "Currently tracking: $TASK\nFrom start: $TIME" "tracking"
+  printf "{\"text\": \"%s\", \"tooltip\": \"%s\", \"alt\": \"%s\"}" \ "$TIME" "Task: $TASK\nFrom start: $TIME" "tracking"
 else
   printf "{\"text\": \"\", \"tooltip\": \"%s\", \"alt\": \"%s\"}" \ "No active tracking at the moment." "idle"
 fi
