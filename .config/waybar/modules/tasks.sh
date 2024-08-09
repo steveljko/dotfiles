4fdd44463f01aacc92e6a6ed08e53b40c24ba2c4
steveljko <steveljko@proton.me>
2024-08-09 11:59:01 +0200

    fix waybar tasks module

--- .config/waybar/modules/tasks.sh
+++ .config/waybar/modules/tasks.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-TASK=$(timew | awk '/Tracking/ {print $2}')
+TASK=$(timew | awk -F'Tracking ' '/Tracking/ {gsub(/"/, "", $2); print $2}')
 
 if [[ -n "$TASK" ]]; then
   TIME=$(timew | awk '/Total/ {print $2}')
   printf "{\"text\": \"%s\", \"tooltip\": \"%s\", \"alt\": \"%s\"}" \ "$TIME" "Task: $TASK\nFrom start: $TIME" "tracking"
 else
-  TASKS_COUNT=$(task status:pending count)
-  TASKS_COMPLETED_COUNT=$(task end.after:today count)
-  printf "{\"text\": \"%s/%s\", \"tooltip\": \"%s\", \"alt\": \"%s\"}" \ "$TASKS_COUNT" "$TASKS_COMPLETED_COUNT" "Completed tasks: $TASKS_COMPLETED_COUNT\nUnfinished tasks: $TASKS_COUNT" "idle"
+  PENDING_TASKS=$(task status:pending count)
+  FINISHED_TASKS=$(task end:today status:completed count)
+  TOTAL_TASKS=$(( $FINISHED_TASKS + $PENDING_TASKS )) 
+  printf "{\"text\": \"%s/%s\", \"tooltip\": \"%s\", \"alt\": \"%s\"}" \ "$FINISHED_TASKS" "$TOTAL_TASKS" "Tasks for today: $TOTAL_TASKS\nFinished tasks: $FINISHED_TASKS\nUnfinished tasks: $PENDING_TASKS" "idle"
 fi
